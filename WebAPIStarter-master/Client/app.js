4aa7e66502940654d88e66358433e88be7cd0a97
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-12-06 10:07:00 -0600

    fixing syntax in app.js

--- WebAPIStarter-master/Client/app.js
+++ WebAPIStarter-master/Client/app.js
@@ -1,4 +1,4 @@
-(function($){
+$(function(){
     function postNewMovie( e ){
         var dict = {
         	Title : this["title"].value,
@@ -27,25 +27,50 @@
 
 
 
-    $(function getAllMovies(){
-
-      $.ajax({
-        type: 'get',
-        url: 'https://localhost:44352/api/movie',
-        dataType: 'json',
-        success: function(data){
-            // $('.tableBody').empty();
-          // $('#allMoviesTable').append("<tr><th>" + Title+ "</th><th>" + Genre+ "</th><th>"+ Director+ "</th></tr>");
-          $.each(data, function(index, value){
-            $('.tableBody').append(
-              '<tr><td>' + value.Title +'</td>,<td>' + value.Genre +'</td>,<td>' +  value.Director + '</td></tr>'
-            )
-          })
-        }
-      });
+    function getAllMovies()
+    {
+        $.ajax({
+            url: 'http://localhost.44352/api/movie',
+            type: 'get',
+            dataType: 'json',
+            success: function (data) {
+                WriteTable(data)
+            }
+        });
     }
-        $('#allMoviesTable').submit( processForm );
+    ​   $('#allMoviesTable').submit(processForm);
+    ​
+    function WriteTable(data) {
+        result = "<table><th>Title</th><th>Genre</th><th>Director</th>";
+        $.each(data, function (index, value) {
+            result += "<tr><td>" + value.Title + "</td><td>" + value.Genre + "</td><td>" + value.Director + "</td>"
+        });
+        result += "</table>"
+        $("#allMoviesTable").html(result);
+    }
+
+  
+
+
+    // $(function getAllMovies(){
+
+    //   $.ajax({
+    //     type: 'get',
+    //     url: 'https://localhost:44352/api/movie',
+    //     dataType: 'json',
+    //     success: function(data){
+    //         // $('.tableBody').empty();
+    //       // $('#allMoviesTable').append("<tr><th>" + Title+ "</th><th>" + Genre+ "</th><th>"+ Director+ "</th></tr>");
+    //       $.each(data, function(index, value){
+    //         $('.tableBody').append(
+    //           '<tr><td>' + value.Title +'</td>,<td>' + value.Genre +'</td>,<td>' +  value.Director + '</td></tr>'
+    //         )
+    //       });
+    //     }
+    //   });
+    // }
+    //     $('#allMoviesTable').submit(processForm);
 
 })(jQuery);
 
-//$.post("From Body", movie, Post(Movie movie))
+// $.post("From Body", movie, Post(Movie movie))
