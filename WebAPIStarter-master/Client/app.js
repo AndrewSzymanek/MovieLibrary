0c7e28f17abe041dd3ec0d0a15805a13500bc64b
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-12-09 07:04:27 -0600

    Final commit before submitting project

--- WebAPIStarter-master/Client/app.js
+++ WebAPIStarter-master/Client/app.js
@@ -1,53 +1,75 @@
-$(function(){
-    function postNewMovie( e ){
-        var dict = {
-        	Title : this["title"].value,
-          Genre : this["genre"].value,
-        	Director: this["director"].value
-        };
+$(document).ready(function(){
 
-        $.ajax({
-            url: 'https://localhost:44352/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
+    // test jquery
+    $("#testButton").click(function testMethod(){
+        $("#test").hide();
+      });
+    // end test jquery
 
-        e.preventDefault();
-    }
-
-    $('#my-form').submit( processForm );
-
-
-
-    function getAllMovies()
-    {
+    //get jquery
+    $("getAllMovies").click(function getAllMovies(){
         $.ajax({
             url: 'http://localhost.44352/api/movie',
             type: 'get',
             dataType: 'json',
-            success: function (data) {
+            sucess: function (data) {
                 WriteTable(data)
             }
-        });
-    }
-    ​   $('#allMoviesTable').submit(processForm);
-    ​
+        })
+    });
+
+    // ​   $('#allMoviesTable').submit(getAllMovies);
+
+    //write table function
     function WriteTable(data) {
-        result = "<table><th>Title</th><th>Genre</th><th>Director</th>";
+        var result = "<table><th>Title</th><th>Genre</th><th>Director</th>";
         $.each(data, function (index, value) {
-            result += "<tr><td>" + value.Title + "</td><td>" + value.Genre + "</td><td>" + value.Director + "</td>"
+            result += "<tr><td>" + value[index].Title + "</td><td>" + value[index].Genre + "</td><td>" + value[index].Director + "</td>"
         });
         result += "</table>"
-        $("#allMoviesTable").html(result);
-    }
+        $("#tableBody").html(result);
+    };
+
+
+
+})(jQuery);
+    
+
+
+    
+    
+    ​
+
+
+    // function postNewMovie( e ){
+    //     var dict = {
+    //     	Title : this["title"].value,
+    //       Genre : this["genre"].value,
+    //     	Director: this["director"].value
+    //     };
+
+    //     $.ajax({
+    //         url: 'https://localhost:44352/api/movie',
+    //         dataType: 'json',
+    //         type: 'post',
+    //         contentType: 'application/json',
+    //         data: JSON.stringify(dict),
+    //         success: function( data, textStatus, jQxhr ){
+    //             $('#response pre').html( data );
+    //         },
+    //         error: function( jqXhr, textStatus, errorThrown ){
+    //             console.log( errorThrown );
+    //         }
+    //     });
+
+    //     e.preventDefault();
+    // }
+
+    // $('#my-form').submit( processForm );
+
+
+
+   
 
   
 
@@ -71,6 +93,6 @@ $(function(){
     // }
     //     $('#allMoviesTable').submit(processForm);
 
-})(jQuery);
+
 
 // $.post("From Body", movie, Post(Movie movie))
